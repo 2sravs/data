ab4f855f9b3f76f75d633eb0277102482fd6b2bc
2Sravs <sravani.chekuri1243#gmail.com>
2023-07-11 11:46:53 +0530

    secondcommit

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Consoledata
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = 5;//value type
+            Console.WriteLine(num);
+            object obj; //reference type
+            obj = num;//boxing
+            Console.WriteLine(obj);
+            int num2;
+            num2 = (int)obj;//unboxing
+            Console.ReadKey();
+        }
+    }
+}
